28f39c80c1c2f8e0e1bf2e78a19cb63f9769c3dc
abc77r <111243752+abc77r@users.noreply.github.com>
2024-08-08 14:06:00 +0300

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,9 @@
+let img= document.querySelector('.img');
+let containr= document.querySelector('.containr');
+
+function phones(phone){
+    img.src =phone;
+}
+function colors(color){
+    containr.style.background =color;
+}
